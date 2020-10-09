c7f01302d8a53b13551d2e175257cf293580f9c7
lin_zhaoxi <lin_zhaoxi@sohu.com>
2020-10-09 17:46:16 +0800

    🍤🍤🍤启动设置

--- src/main/java/win/pangniu/learn/App.java
+++ src/main/java/win/pangniu/learn/App.java
@@ -1,13 +1,8 @@
 package win.pangniu.learn;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import win.pangniu.learn.service.StorageService;
 
 /**
  * 启动类
@@ -15,23 +10,10 @@ import win.pangniu.learn.service.StorageService;
  * version 1.0
  */
 @SpringBootApplication
-@ComponentScan
-@EnableAutoConfiguration
 public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    CommandLineRunner init(final StorageService storageService) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                storageService.deleteAll();
-                storageService.init();
-            }
-        };
     }
 
 }
